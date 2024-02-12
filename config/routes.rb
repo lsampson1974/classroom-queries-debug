963369613e4f37953d9af84e2be112b8bd4433c1
Lamont Sampson <148111503+lsampson1974@users.noreply.github.com>
2024-02-12 17:49:23 +0000

    Completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -25,9 +25,9 @@ Rails.application.routes.draw do
 
   post("/insert_course", { :controller => "courses", :action => "create" })
 
-  get("/courses", { :controller => "courses_controller", :action => "index" })
+  get("/courses", { :controller => "courses", :action => "index" })
   
-  get("/modify_course/:path_id", { :controller => "courses", :action => "update" })
+  post("/modify_course/:path_id", { :controller => "courses", :action => "update" })
 
   get("/delete_course/:path_id", { :controller => "courses", :action => "destroy" })
 
@@ -48,6 +48,6 @@ Rails.application.routes.draw do
 
   #------------------------------
 
-  post("/insert_enrollment", { :controller => "enrollments", :action => "create" })
+  post("/insert_enrollment", { :controller => "enrollment", :action => "create" })
 
 end
